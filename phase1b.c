540f7484783ed48676724b79619cd8c81f4e3bd1
pranavt22 <pranavtalwar@email.arizona.edu>
2021-02-09 17:36:23 -0700

    test

--- phase1b.c
+++ phase1b.c
@@ -15,29 +15,40 @@ typedef struct PCB {
     char            name[P1_MAXNAME+1]; // process's name
     int             priority;           // process's priority
     P1_State        state;              // state of the PCB
+    int             children[P1_MAXPROC];
+    int             numOfChildren;
+    int             parentID;
+
     // more fields here
+    // who your parent is (processid)
+
 } PCB;
 
 static PCB processTable[P1_MAXPROC];   // the process table
+static int currProcessID = 0;
 
-void P1ProcInit(void)
-{
+void launch(void) {
+    assert(contexts[currentCid].startFunc != NULL);
+    P1_Quit(processTable[currentPid].startFunc(args));
+}
+
+void P1ProcInit(void) {
     P1ContextInit();
     for (int i = 0; i < P1_MAXPROC; i++) {
         processTable[i].state = P1_STATE_FREE;
+        processTable[i]
         // initialize the rest of the PCB
+        // when allocating a new control block you need to see whats free
     }
     // initialize everything else
 
 }
 
-int P1_GetPid(void) 
-{
-    return 0;
+int P1_GetPid(void) {
+    return currProcessID;
 }
 
-int P1_Fork(char *name, int (*func)(void*), void *arg, int stacksize, int priority, int *pid ) 
-{
+int P1_Fork(char *name, int (*func)(void*), void *arg, int stacksize, int priority, int *pid ) {
     int result = P1_SUCCESS;
 
     // check for kernel mode
@@ -51,9 +62,7 @@ int P1_Fork(char *name, int (*func)(void*), void *arg, int stacksize, int priori
     return result;
 }
 
-void 
-P1_Quit(int status) 
-{
+void P1_Quit(int status) {
     // check for kernel mode
     // disable interrupts
     // remove from ready queue, set status to P1_STATE_QUIT
@@ -66,32 +75,28 @@ P1_Quit(int status)
 }
 
 
-int 
-P1GetChildStatus(int *cpid, int *status) 
-{
+int P1GetChildStatus(int *cpid, int *status) {
     int result = P1_SUCCESS;
     // do stuff here
     return result;
 }
 
-int
-P1SetState(int pid, P1_State state, int lid, int vid) 
-{
+// LOOK AT PHASE1.H LINE 51
+// PROCESS CONTROL BLOCK CAN BE IN 1 OF 6 STATES AND IT CAN TRANSITION.
+// WHAT SHOULD WE DO IF A SPECIFIC TRANSITION OCCURS.
+// IF ITS RUNNING STATE AND IT CALLS QUIT, THEN IT WILL GO QUIT
+int P1SetState(int pid, P1_State state, int lid, int vid) {
     int result = P1_SUCCESS;
     // do stuff here
     return result;
 }
 
-void
-P1Dispatch(int rotate)
-{
+void P1Dispatch(int rotate) {
     // select the highest-priority runnable process
     // call P1ContextSwitch to switch to that process
 }
 
-int
-P1_GetProcInfo(int pid, P1_ProcInfo *info)
-{
+int P1_GetProcInfo(int pid, P1_ProcInfo *info) {
     int         result = P1_SUCCESS;
     // fill in info here
     return result;
